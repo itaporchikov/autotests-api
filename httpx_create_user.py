8d223df20d9867b003b648a6c2c1c51866001cc7
itaporchikov <ilya.taporchikov23@gmail.com>
2025-11-21 19:27:39 +0300

    Практикуемся в работе с HTTPX

---
+++ httpx_create_user.py
@@ -0,0 +1,14 @@
+import httpx
+from tools.fakers import get_random_email
+
+payload = {
+    "email": get_random_email(),
+    "password": "Test12345",
+    "lastName": "Uno",
+    "firstName": "Savage",
+    "middleName": "leo"
+}
+response = httpx.post("http://localhost:8000/api/v1/users", json=payload)
+
+print(response.status_code)
+print(response.json())
